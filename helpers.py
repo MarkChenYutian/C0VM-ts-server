89c3779090293c3037a2aac478023422b0217d95
Yutian Chen <yutianch@andrew.cmu.edu>
2022-08-04 00:03:52 -0400

    :zap: Switch to FastAPI and support async request

---
+++ helpers.py
@@ -0,0 +1,67 @@
+import os
+import shutil
+
+import asyncio
+from typing import List, Tuple
+
+async def execute_command(program: str, *args: List[str]) -> Tuple[str, str, int]:
+    """
+    Execute command `$ program *args` in an async manner 
+    with subprocess in the background.
+
+    Return a tuple of form `(stdout, stderr, retval)`
+    """
+
+    print(args)
+
+    # Create an async subprocess from current process
+    child_proc = await asyncio.create_subprocess_exec(
+        program,
+        *args,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+
+    # Receive stdout, stderr from child process
+    stdout, stderr = await child_proc.communicate()
+
+    return_val = child_proc.returncode
+    stdout_str = stdout.decode().strip()
+    stderr_str = stderr.decode().strip()
+
+    return (stdout_str, stderr_str, return_val)
+
+
+def path_sanitizer(file_name: str) -> str:
+    """
+    Sanitize file names to prevent the file names like "../../../config" 
+    being injected to the server
+    """
+    return file_name.split("/")[-1]
+
+
+def create_workspace(session_id: str, file_names: List[str], codes: List[str]) -> None:
+    """
+    Initialize the workspace for session
+    """
+    os.mkdir(f"./cache/{session_id}")
+
+    for file_name, code in zip(file_names, codes):
+        with open(f"./cache/{session_id}/{file_name}", "w") as F:
+            F.write(code)
+    return
+
+def destroy_workspace(session_id: str) -> None:
+    """
+    Destroy the session workspace to release resources
+    """
+    shutil.rmtree(f"./cache/{session_id}")
+    return
+
+if __name__ == "__main__":
+    asyncio.run(
+        execute_command(
+            "cc0",
+            *['-b', '-o', './cache/f7acaeaa-3288-4a62-821e-39d1aeba9657/out.bc0', './cache/f7acaeaa-3288-4a62-821e-39d1aeba9657/something.c0'],
+        )
+    )
---
+++ main.py
@@ -0,0 +1,69 @@
+from sys import stdout
+import uuid
+import asyncio
+
+from typing import List
+from helpers import execute_command, create_workspace, destroy_workspace, path_sanitizer
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from strip_ansi import strip_ansi
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/compile")
+async def compile_project(filenames: List[str], codes: List[str], dyn_check: bool):
+    """
+    Compile C0 source code to BC0 bytecode
+    """
+    session_id = str(uuid.uuid4())
+    filenames = list(map(path_sanitizer, filenames))
+
+    create_workspace(session_id, filenames, codes)
+
+    try:
+        compile_filenames = list(map(lambda s: f"./cache/{session_id}/" + s, filenames))
+        output_filename = f"./cache/{session_id}/out.bc0"
+        args = []
+
+        if (dyn_check):
+            args.append("-d")
+        args.append("-b")               # set bytecode compiler
+        args.append("-o")               # set output file name
+        args.append(output_filename)    # set output_file
+        args += compile_filenames       # compile files in given order
+
+        stdout_str, stderr_str, retval = await execute_command(
+            "cc0", *args
+        )
+
+        stdout_str = strip_ansi(stdout_str)
+        
+        if (retval == 0):
+            with open(output_filename, "r") as F:
+                bytecode_content = F.read()
+            destroy_workspace(session_id)    
+            return {
+                "bytecode": bytecode_content,
+                "error": ""
+            }
+        else:
+            destroy_workspace(session_id)
+            return {
+                "bytecode": "",
+                "error": stdout_str
+            }
+        
+    except Exception as e:
+        destroy_workspace(session_id)
+        raise e
+
